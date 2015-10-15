c0121214a30833383e28d89b93f14409df8b0ceb
rort1989 <zhaorort@gmail.com>
2015-10-15 12:20:24 -0400

    select sequences

--- script_result.m
+++ script_result.m
@@ -1,18 +1,48 @@
 %% scripts to generate plot, analyze results
-src = load('McMaster/McMaster.mat');
-inst = 4; % 14 % select one sequence
-idx_au = 1;
-dfactor = 5;
-% downsampledata(data,intensity,dfactor);
-data = src.features{inst}(1:dfactor:end,:)';
-data = data(99:end,:); 
-intensity = src.AU{inst}(1:dfactor:end,idx_au);
-[labels(2),labels(1)] = max(intensity);
-xx=load('BP4D/results/model2_3.mat');
-dec_values = xx.theta'*[data; ones(1,size(data,2))];
-RR = corrcoef(dec_values,intensity);  ry = RR(1,2);
+% selected sequence for each subject
+inst{1} = [6]; % ex1
+inst{2} = [5]; % ex1
+inst{3} = [1 4]; % ex1
+inst{4} = [2 6]; % ex1
+inst{5} = [3 5 7]; % ex1 inst 1,2
+inst{6} = [1 8]; % ex1
+inst{7} = [];
+inst{8} = [2 3]; % ex1
+inst{9} = [8]; % ex1
+inst{10} = [2:7]; % ex1 inst 2,5
+inst{11} = [2 5];
+inst{12} = [3]; % ex1
+inst{13} = [5]; % ex1
+inst{14} = [1 2 5]; % ex1 inst 1
+inst{15} = [];
+inst{16} = [1 5 7 9 11 13 15]; % ex1 inst 1,9
+inst{17} = [1];
+inst{18} = [1 4]; % ex1 - inst4
+inst{19} = [3 4 10];
+inst{20} = [2 6];
+inst{21} = [1 3 5]; % ex1 - inst3
+inst{22} = [1];
+inst{23} = [];
+inst{24} = [2 6];
+inst{25} = [1 5]; % ex1
 
-subplot(2,1,1)
-plot(intensity); title('intensity level of selected AU')
-subplot(2,1,2)
-plot(dec_values); title('intensity from learned regression model')
+% plot the pain intensity changes
+close all;
+src = load('McMaster/McMaster.mat');
+for s = 1:numel(src.subject)
+    PSPI = src.PSPI{s};
+    figure
+    for n = 1:numel(inst{s})%numel(PSPI)
+        seq = PSPI{inst{s}(n)}; %seq = PSPI{n};
+        subplot(1,numel(inst{s}),n);%subplot(2,ceil(numel(PSPI)/2),n);
+        plot(seq);
+        title(num2str(n));
+    end
+end
+
+
+
+% subplot(2,1,1)
+% plot(intensity); title('intensity level of selected AU')
+% subplot(2,1,2)
+% plot(dec_values); title('intensity from learned regression model')
